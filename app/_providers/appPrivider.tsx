710912290cf1d05f500f3081bc7cfc21b5148956
= <=>
2024-10-23 07:40:45 +0600

    delete pwa

--- app/_providers/appPrivider.tsx
+++ app/_providers/appPrivider.tsx
@@ -1,11 +1,11 @@
 'use client'
 import AppSessionProvider from '@front/features/auth/appSessionProvider'
 import ThemeProvider from '@front/kernel/lib/next-theme/themeProvider'
-import { Session } from 'next-auth'
-import React, { useEffect } from 'react'
 import { Toaster } from '@front/shared/ui/sonner'
-import QueriesClient from './queriesClient'
 import { TooltipProvider } from '@radix-ui/react-tooltip'
+import { Session } from 'next-auth'
+import React from 'react'
+import QueriesClient from './queriesClient'
 
 const AppProvider = ({
 	children,
@@ -14,19 +14,6 @@ const AppProvider = ({
 	children: React.ReactNode
 	session: Session | null | undefined
 }) => {
-	useEffect(() => {
-		if ('serviceWorker' in navigator) {
-			navigator.serviceWorker.register('/sw.js').then(
-				registration => {
-					console.log('Service Worker registered with scope:', registration.scope)
-				},
-				error => {
-					console.error('Service Worker registration failed:', error)
-				}
-			)
-		}
-	}, [])
-
 	return (
 		<ThemeProvider>
 			<AppSessionProvider session={session}>
