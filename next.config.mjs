cce9f60d89e38a274be590e841cdbf37a093cb4c
= <=>
2024-10-23 07:34:31 +0600

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -6,38 +6,33 @@ const nextConfig = {
 				source: '/(.*)',
 				headers: [
 					{
-						source: '/(.*)',
-						headers: [
-							{
-								key: 'X-Content-Type-Options',
-								value: 'nosniff'
-							},
-							{
-								key: 'X-Frame-Options',
-								value: 'DENY'
-							},
-							{
-								key: 'Referrer-Policy',
-								value: 'strict-origin-when-cross-origin'
-							}
-						]
+						key: 'X-Content-Type-Options',
+						value: 'nosniff'
 					},
 					{
-						source: '/sw.js',
-						headers: [
-							{
-								key: 'Content-Type',
-								value: 'application/javascript; charset=utf-8'
-							},
-							{
-								key: 'Cache-Control',
-								value: 'no-cache, no-store, must-revalidate'
-							},
-							{
-								key: 'Content-Security-Policy',
-								value: "default-src 'self'; script-src 'self'"
-							}
-						]
+						key: 'X-Frame-Options',
+						value: 'DENY'
+					},
+					{
+						key: 'Referrer-Policy',
+						value: 'strict-origin-when-cross-origin'
+					}
+				]
+			},
+			{
+				source: '/sw.js',
+				headers: [
+					{
+						key: 'Content-Type',
+						value: 'application/javascript; charset=utf-8'
+					},
+					{
+						key: 'Cache-Control',
+						value: 'no-cache, no-store, must-revalidate'
+					},
+					{
+						key: 'Content-Security-Policy',
+						value: "default-src 'self'; script-src 'self'"
 					}
 				]
 			}
